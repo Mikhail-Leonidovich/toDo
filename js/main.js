146443fadd6676514c567c88863b1e2d435d6c39
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-08-12 12:22:33 +0300

    v 1.07

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const block = document.querySelector(".block");
-const btnAdd = document.querySelector(".btn-add");
+
+const blockAdd = document.querySelector(".block__add");
 const inputText = document.querySelector(".block__add-text");
+const btnAdd = document.querySelector(".btn-add");
+
 const blockList = document.querySelector(".block__list");
 const blockListText = document.querySelector(".block__list-text");
 const btnDel = document.querySelector(".btn-del");
@@ -8,23 +11,29 @@ const btnDone = document.querySelector(".btn-done");
 
 const arrayOfTasks = [];
 
-inputText.addEventListener("keydown", (e) => {
-  if (e.keyCode === 13) {
-    e.preventDefault();
+block.addEventListener("keydown", (e) => {
+  if (e.target.matches(".block__add-text")) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      addNewBlockListElem(inputText.value);
+      clearInputText();
+    }
+  }
+});
+
+block.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-add")) {
     addNewBlockListElem(inputText.value);
     clearInputText();
   }
 });
 
-btnAdd.addEventListener("click", () => {
-  addNewBlockListElem(inputText.value);
-  clearInputText();
-});
-
-const clearInputText = () => (inputText.value = "");
+const clearInputText = () => {
+  inputText.value = "";
+};
 
 const addNewBlockListElem = (elem) => {
-  if (!elem && elem == "") {
+  if (elem == "") {
     alert("Вы не заполнили поле");
     return;
   } else {
@@ -41,8 +50,6 @@ const addInArrayOfTasks = (elem) => {
   arrayOfTasks.push(newToDo);
 };
 
-/* ДОДЕЛАТЬ */
-
 const displayTask = () => {
   let test = "";
 
@@ -55,15 +62,14 @@ const displayTask = () => {
     </div>
     `;
   });
-  block.innerHTML = `
-    <div class="block__add">
+
+  block.innerHTML = `<div class="block__add">
       <input
         class="block__add-text"
         type="text"
         placeholder="Введите заметку"
       />
-      <button class="btn-add">Add</button>
+      <button type="button" class="btn-add">Add</button>
     </div>  
-    ${test}
-`;
+    ${test}`;
 };
