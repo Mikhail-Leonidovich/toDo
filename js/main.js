6380622973b44adac1fbd9e00196c03454905a9d
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-08-15 14:25:44 +0300

    added deletion and marks

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const btnAdd = document.querySelector(".btn-add");
 
 const btnDel = document.querySelector(".btn-del");
 const btnDone = document.querySelector(".btn-done");
+const blockListText = document.querySelector(".block__list-text");
 
 const arrayOfTasks = [];
 
@@ -51,15 +52,62 @@ const addInArrayOfTasks = (elem) => {
 const displayTask = () => {
   let blockNewInner = "";
 
+  /* КОСТЫЛЬ */
+
   arrayOfTasks.forEach((item, i) => {
-    blockNewInner += `
-    <div class="block__list" style="display : flex">
-            <div id="#${i + 1}" class="block__list-text">${item.todo}</div>
-            <button class="btn-del">Delete</button>
-            <button class="btn-done">Done</button>
-    </div>
-    `;
+    if (item.checked == false) {
+      blockNewInner += `
+      <div id="${i + 1}" class="block__list" style="display : flex">
+              <div id="${i + 1}" class="block__list-text">${item.todo}</div>
+              <button id="${i + 1}" class="btn-del">Delete</button>
+              <button id="${i + 1}" class="btn-done">Done</button>
+      </div>`;
+    } else if (item.checked == true) {
+      blockNewInner += `
+      <div id="${i + 1}" class="block__list" style="display : flex">
+              <div id="${
+                i + 1
+              }" class="block__list-text" style="background-color: rgb(106, 224, 106)">${
+        item.todo
+      }</div>
+              <button id="${i + 1}" class="btn-del">Delete</button>
+              <button id="${i + 1}" class="btn-done">Done</button>
+      </div>`;
+    }
   });
 
   blockNew.innerHTML = blockNewInner;
 };
+
+block.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-del")) {
+    let currentBtnDel = e.target.id;
+
+    arrayOfTasks.forEach((item, i) => {
+      if (i == currentBtnDel - 1) {
+        /* arrayOfTasks.splice(i, 1); */
+
+        delete arrayOfTasks[i]; /* КОСТЫЛЬ */
+
+        let allBlockLists = document.querySelectorAll(".block__list");
+
+        for (let element of allBlockLists) {
+          if (element.id == currentBtnDel) {
+            element.remove(this);
+          }
+        }
+      }
+    });
+  } else if (e.target.matches(".btn-done")) {
+    let currentBtnDone = e.target.id;
+
+    let allBlockListTexts = document.querySelectorAll(".block__list-text");
+
+    for (let element of allBlockListTexts) {
+      if (element.id == currentBtnDone) {
+        element.style = "background-color: rgb(106, 224, 106)";
+        arrayOfTasks[currentBtnDone - 1].checked = true;
+      }
+    }
+  }
+});
